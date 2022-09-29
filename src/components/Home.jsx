9e0c13eadaf176dae18160f91a01969eb7de6926
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2022-09-29 21:15:58 +0530

    Search filter add and updated home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import "./styles/Home.css"
+import { useNavigate } from "react-router";
 
 const Home = () => {
+    const [search, setSearch] = useState("");
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios
+          .get("https://brochhill12.herokuapp.com/food")
+          .then((res) => {
+            setData(res.data);
+          })
+          .then((e) => {
+            handleDelete(e);
+          });
+      }, [data]);
+
+      const handleDelete = (id) => {
+        fetch(`https://brochhill12.herokuapp.com/food/${id}`, {
+          method: "DELETE",
+        }).then((result) => {
+          result.json();
+        });
+      };
+   
+      const handleSingleFood = (event) => {
+        localStorage.setItem("singleFood", JSON.stringify(event));
+        navigate("/singleFood");
+      };
+    
+    
   return (
     <div>
-       
+        <div className="home">
+      <div className="search-main1">
+      <form className="d-flex" role="search">
+      <input className="form-control me-0" type="search" placeholder="Search" aria-label="Search"    onChange={(e) => {
+            setSearch(e.target.value);
+          }}/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+      </div>
+      <div className="content">
+        {data
+          ? data
+              .filter((e) => e.name.toLowerCase().includes(search))
+              .map((el) => {
+                return (
+                  <div
+                    className="box"
+                  >
+                    <div>
+                      <img 
+                        src={el.img} 
+                        alt="" 
+                        onClick={() => {
+                            handleSingleFood(el);
+                        }}
+                    />
+                    </div>
+                    <div onClick={() => {
+                      handleSingleFood(el);
+                    }}>{el.name}</div>
+                    <div onClick={() => {
+                      handleSingleFood(el);
+                    }}>{el.ingredients}</div>
+                    <div>
+                   
+                      <button onClick={() => handleDelete(el._id)}>
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                );
+              })
+          : [...Array(8)].map((data) => {
+              return (
+                <div className="box">
+                  <div>
+                    <Skeleton
+                        circle
+                    />
+                  </div>
+                  <div><Skeleton /></div>
+                  <div><Skeleton /></div>
+                </div>
+              );
+            })}
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
-
+import './styles/Home.css'
 const Navbar = () => {
+    const [search, setSearch] = useState("");
   return (
     <div>
          <nav className="navbar bg-light">
   <div className="container-fluid">
     <Link to="/">
-    <a className="navbar-brand">BRO-Chill</a>
+    <a className="navbar-brand" id='a'>BRO-Chill</a>
     </Link>
     <Link to="/add">
-    <a className="navbar-brand">Add Image</a>
+    <a className="navbar-brand" id='a'>Add Image</a>
     </Link>
-    <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success" type="submit">Search</button>
-    </form>
+
   </div>
 </nav>
     </div>
